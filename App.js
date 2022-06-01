8c36bf82446264ba4683a7deff47f1965254964a
danis <amaury.danis-cousandier@epitech.eu>
2022-06-01 21:54:22 +0200

    #6 - import navigation container

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
 
 import { SplashScreen } from './src/SplashScreen';
 
 export default function App() {
+  const nav = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <SplashScreen></SplashScreen>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <nav.Navigator>
+        <nav.Screen options={{headerShown: false}} name='SplashScreen' component={SplashScreen}/>
+      </nav.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <SplashScreen></SplashScreen>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
