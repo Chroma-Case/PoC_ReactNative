b127ee8a29af9ce1e8552837b237f1c38782c4dd
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-07 14:32:32 +0200

    #11 - Create a User Model

---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+import { UserCompetencies } from "./user-competencies";
+
+/// The identity the app user will be authentified as
+export default class User {
+	constructor(
+		/// The display name of the user
+		public name: string,
+		/// The register name of the user, used to log into the app
+		public login: string,
+		/// count of notes correctly played over his course
+		public goodNotes: number,
+		/// record of the user's competencies
+		public competencies: UserCompetencies
+	) {}
+	
+	get level() {
+		return this.goodNotes / 10000;
+	}
+}
