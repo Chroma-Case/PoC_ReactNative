d62de5714f1223bd41caf4461d9f88e646662018
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-09 21:55:49 +0200

    Splash screen: align items + fit image

--- src/views/splashscreen.tsx
+++ src/views/splashscreen.tsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Button from '../components/button';
+import FitImage from 'react-native-fit-image';
+import logo from '../../assets/banner.png';
 
 export const SplashScreen = ({ navigation }) => {
 	return (
-		<View>
-			{/* <Image source={{ uri: '../assets/banner.jpeg' }} style={{width: 400, height: 400}}/> */}
-			<Button label={'Login'} onPressed={() => navigation.navigate('Home')} color={'green'}/>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly'}}>
+			<FitImage
+				style={{ height: 200, width: 300 }}
+				resizeMode='center'
+				source={logo}
+			/>
+			<View style={{ alignItems: 'center', justifyContent: 'center' }}>
+				<Button label={'Login'} onPressed={() => navigation.navigate('Home')} color={'green'} />
+			</View>
 		</View>
 	);
 }
