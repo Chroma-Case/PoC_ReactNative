fdf133b0052bd52d18b739ee6ced9d09e4d7426f
Arthi-chaud <arthur.jamet@gmail.com>
2022-06-10 11:49:09 +0200

    Home Screen: First scroll view

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import Button from '../components/button';
 import TrackCard from '../components/track-card';
+
+const styles = StyleSheet.create({
+	trackGroupHeader: {
+		fontWeight: 'bold',
+		padding: 20
+	}
+})
 
 export const HomeScreen = () => {
 	return (
 		<View>
-			<Text style={{ fontSize: 30, fontWeight: 'bold', padding: 20 }}>Welcome John</Text>
-			<TrackCard/>
+			<Text style={{ fontSize: 30, fontWeight: 'bold', padding: 20 }}>Bienvenue, John!</Text>
+			<Text style={styles.trackGroupHeader}>Passer à l'étape supérieure</Text>
+			<ScrollView style={{ paddingVertical: 10 }} horizontal={true} contentContainerStyle={{  }}>
+				{Array.from({ length: 10 }, (_, i) => 
+				<View style={{paddingHorizontal: 10}}>
+					<TrackCard key={i}/>
+				</View>)}
+			</ScrollView>
+			
 		</View>
 	);
 }
